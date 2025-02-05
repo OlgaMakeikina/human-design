c8e973bf5cd277598df902ac32e63f54e0ea0155
OlgaMakeikina <makeikina2202@gmail.com>
2025-02-05 09:14:49 -0300

    тест

--- script.js
+++ script.js
@@ -231,12 +231,3 @@ requestAnimationFrame(update);
 
 
 
-document.addEventListener('touchmove', function(event) {
-  if (window.scrollY <= 0) {
-    event.preventDefault();
-  }
-}, { passive: false });
-
-
-
-
